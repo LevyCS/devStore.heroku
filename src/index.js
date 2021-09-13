8f068d396ac3e17207ddee3afb2e368c766dd3dd
LevyCS <levyfronto@gmail.com>
2021-09-13 18:08:23 -0300

    finalizado

--- src/index.js
+++ src/index.js
@@ -16,14 +16,45 @@ app.get('/produto', async(req, resp) =>{
 app.post('/produto', async(req, resp) => {
     try {
         let {nomeProduto, categoria, precoDe, precoPor, avaliacao, descricao, quantidadeEstoque, imagemProduto} = req.body;
-            let r = await db.tb_produto.create({nm_produto: nomeProduto, ds_categoria: categoria, vl_preco_de: precoDe, vl_preco_por: precoPor, vl_avaliacao: avaliacao, ds_produto: descricao, qtd_estoque:quantidadeEstoque, img_produto: imagemProduto, bt_ativo: true, dt_inclusao: new Date()})
-            resp.send(r);
-        } catch (e) { {erro: resp.send(e.toString())}}
+
+        if(nomeProduto == '' || categoria == '' || precoDe == '' || precoPor == '' || avaliacao == '' || descricao == '' || quantidadeEstoque == '' || imagemProduto == '')
+           
+        resp.send({erro: "Um ou mais campos não estão preenchidos"})
+
+        if(isNaN(avaliacao) || isNaN(precoDe) || isNaN(precoPor) || isNaN(quantidadeEstoque)) 
+            resp.send({erro: "avaliacao, preco de:, preco por: e Estoque precisam ser números"})
+
+        if (avaliacao < 0 || precoDe < 0 || precoPor < 0 || quantidadeEstoque < 0)
+            resp.send({erro: "avaliacao, preco DE:, preco POR: e Estoque precisam ser números positivos"})
+        
+        let u = await db.tb_produto.findOne({where: {nm_produto: nomeProduto}}) 
+        if (u != null) 
+            resp.send( {erro: "Produto já cadastrado"})
+
+        let r = await db.tb_produto.create({nm_produto: nomeProduto, ds_categoria: categoria, vl_preco_de: precoDe, vl_preco_por: precoPor, vl_avaliacao: avaliacao, ds_produto: descricao, qtd_estoque:quantidadeEstoque, img_produto: imagemProduto, bt_ativo: true, dt_inclusao: new Date()})
+        resp.send(r);
+    } catch (e) { {erro: resp.send(e.toString())}}
 })
 
 app.put('/produto/:id', async(req,resp) => {
     try {
         let {nomeProduto, categoria, precoDe, precoPor, avaliacao, descricao, quantidadeEstoque, imagemProduto} = req.body;
+        if(nomeProduto == '' || categoria == '' || precoDe == '' || precoPor == '' || avaliacao == '' || descricao == '' || quantidadeEstoque == '' || imagemProduto == '')
+            resp.send({erro: "Um ou mais campos não estão preenchidos"})
+
+        if(isNaN(avaliacao) || isNaN(precoDe) || isNaN(precoPor) || isNaN(quantidadeEstoque)) 
+            resp.send({erro: "avaliacao, preco de:, preco por: e Estoque precisam ser números"})
+
+        if (avaliacao < 0 || precoDe < 0 || precoPor < 0 || quantidadeEstoque < 0)
+            resp.send({erro: "avaliacao, preco DE:, preco POR: e Estoque precisam ser números positivos"})
+    
+        let u = await db.tb_produto.findOne({where: {nm_produto: nomeProduto}}) 
+        if (u != null) {
+            if(req.params.id != u.id_produto) {
+                resp.send( {erro: "Produto já cadastrado"})
+            }
+        }
+            
         let r = await db.tb_produto.update({nm_produto: nomeProduto, ds_categoria: categoria, vl_preco_de: precoDe, vl_preco_por: precoPor, vl_avaliacao: avaliacao, ds_produto: descricao, qtd_estoque:quantidadeEstoque, img_produto: imagemProduto, bt_ativo: true}, {where: {id_produto: req.params.id}})
         resp.sendStatus(200)
     } catch(e) {{erro: resp.send(e.toString())}}
